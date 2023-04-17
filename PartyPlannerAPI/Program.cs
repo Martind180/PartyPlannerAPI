8064d6cbdea5b7cdb08c40d5e14d550b726eb814
Martin Dunn <martin.dun176@hotmail.com>
2023-04-17 21:05:28 +0100

    Updated project to use FastEndpoints and Swagger

--- PartyPlannerAPI/Program.cs
+++ PartyPlannerAPI/Program.cs
@@ -1,5 +1,14 @@
+global using FastEndpoints;
+using FastEndpoints.Swagger;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddFastEndpoints();
+builder.Services.AddSwaggerDoc();
+
 var app = builder.Build();
+
+app.UseFastEndpoints();
+app.UseSwaggerGen();
 
 app.Run();
