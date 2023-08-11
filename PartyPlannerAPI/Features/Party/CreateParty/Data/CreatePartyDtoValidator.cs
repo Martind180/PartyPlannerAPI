1cd9dd3fbea6c6056047ea1aa68310039623b43a
Martin Dunn <Martin.dun176@hotmail.com>
2023-08-11 19:50:57 +0100

    Updated create party validation

---
+++ PartyPlannerAPI/Features/Party/CreateParty/Data/CreatePartyDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿namespace PartyPlannerAPI.Features.Party.CreateParty.Data;
+
+public class CreatePartyDtoValidator : Validator<CreatePartyDto>
+{
+    public CreatePartyDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Party name cannot be empty");
+        
+        RuleFor(x => x.PartyInfo)
+            .NotEmpty()
+            .WithMessage("Party info cannot be empty");
+        
+        RuleFor(x => x.Date)
+            .NotNull()
+            .WithMessage("Party date cannot be empty");
+
+        RuleFor(x => x.Venue)
+            .NotNull()
+            .WithMessage("Please provide a venue");
+    }
+}
---
+++ PartyPlannerAPI/Features/Party/CreateParty/Data/CreateVenueDtoValidator.cs
@@ -0,0 +1,39 @@
+﻿namespace PartyPlannerAPI.Features.Party.CreateParty.Data;
+
+public class CreateVenueDtoValidator : Validator<CreateVenueDto>
+{
+    public CreateVenueDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Venue name cannot be empty");
+        
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .WithMessage("Venue description cannot be empty");
+        
+        RuleFor(x => x.AddressLine1)
+            .NotEmpty()
+            .WithMessage("Venue address line 1 cannot be empty");
+        
+        RuleFor(x => x.Town)
+            .NotEmpty()
+            .WithMessage("Venue town cannot be empty");
+            
+        RuleFor(x => x.County)
+            .NotEmpty()
+            .WithMessage("Venue county cannot be empty");
+        
+        RuleFor(x => x.Postcode)
+            .NotEmpty()
+            .WithMessage("Venue postcode cannot be empty");
+            
+        RuleFor(x => x.Longitude)
+            .NotEmpty()
+            .WithMessage("Venue longitude cannot be empty");
+        
+        RuleFor(x => x.Latitude)
+            .NotEmpty()
+            .WithMessage("Venue latitude cannot be empty");
+    }
+}
